bb54e8eac876453376b3a7b8d96d8c30d909c53e
dengjianbin@lesdo.cn <dengjianbin@lesdo.cn>
2019-06-27 10:37:55 +0800

    first commit

---
+++ src/main/java/com/zhengjianbin/algorithm/MyalgorithemexerciseApplication.java
@@ -0,0 +1,13 @@
+package com.zhengjianbin.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyalgorithemexerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyalgorithemexerciseApplication.class, args);
+	}
+
+}
